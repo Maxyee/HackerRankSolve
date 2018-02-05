0aa02a717c3b3827470e19e0333c875164808c9f
Maxyee <eyaminkhan00@gmail.com>
2018-02-06 01:09:11 +0600

    Solved the String Related problem

---
+++ acid_name_solve/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace acid_name_solve
+{
+    class Program
+    {
+        static string acidNaming(string acid_name) 
+        {
+            if (acid_name.Substring(0, 5) == "hydro" && acid_name.Substring(acid_name.Length - 2) == "ic")
+            {
+                //Console.WriteLine("non-metal acid");
+                return "non-metal acid";
+            }
+            else if (acid_name.Substring(acid_name.Length - 2) == "ic")
+            {
+                //Console.WriteLine("polyatomic acid");
+                return "polyatomic acid";
+            }
+            else
+            {
+                //Console.WriteLine("not an acid");
+                return "not an acid";
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < n; a0++)
+            {
+                string acid_name = Console.ReadLine();
+                string result = acidNaming(acid_name);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
