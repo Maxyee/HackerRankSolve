3f37340829e1a6e372ae5c713e49e2030a0d4e8f
Maxyee <eyaminkhan00@gmail.com>
2018-02-06 12:20:09 +0600

    Solved how to sum all of the array data

---
+++ DataStructureArraySum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureArraySum
+{
+    class Program
+    {
+        static int simpleArraySum(int n, int[] ar)
+        {
+            int res = ar.Sum();
+            return res;
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int result = simpleArraySum(n, ar);
+            Console.WriteLine(result);
+            //Console.ReadKey();
+        }
+    }
+}
