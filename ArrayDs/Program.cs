9a03e58029394aa68be152828d7d1b90f2479a80
Maxyee <eyaminkhan00@gmail.com>
2018-02-06 17:27:02 +0600

    Learned how to reverse a Array data

---
+++ ArrayDs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDs
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            Array.Reverse(arr);
+            //Console.WriteLine(string.Join(" ", arr));
+            for (int i = 0; i < n; i++)
+            {
+                //Console.WriteLine(arr[i]);
+                //Console.WriteLine(string.Join(" ", arr));
+                //Console.Write("{0}", arr[i]);
+                Console.Write(arr[i] + " ");
+            }
+            //Console.WriteLine(arr);
+            Console.ReadKey();
+        }
+    }
+}
