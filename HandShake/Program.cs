abdea7baf75bdb2d6f2aa729c39fda7e4edec0d2
Maxyee <eyaminkhan00@gmail.com>
2018-02-08 03:22:55 +0600

    HandShake Problem done

---
+++ HandShake/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandShake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < T; a0++)
+            {
+                int N = Convert.ToInt32(Console.ReadLine());
+                int result = N*(N - 1)/2;
+
+                Console.WriteLine("" +result);  
+            }
+            Console.ReadKey();
+        }
+    }
+}
