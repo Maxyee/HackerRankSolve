b1a045122b2f3d533da0fcaf2cb07e5789b448d4
Maxyee <eyaminkhan00@gmail.com>
2018-02-08 02:57:27 +0600

    learned the worst case senario for finding x numbers of shocks

---
+++ MaximumDraws/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumDraws
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                int N = Convert.ToInt32(Console.ReadLine());
+                int M = N + 1;
+                Console.WriteLine(""+M);              
+            }
+        }
+    }
+}
