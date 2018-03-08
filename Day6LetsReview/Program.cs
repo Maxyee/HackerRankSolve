762b9db0ddbbf44b1300a31a40ca6862354b4605
Maxyee <eyaminkhan00@gmail.com>
2018-03-09 05:24:51 +0600

    Solved the day6 problem of hackerrank

---
+++ Day6LetsReview/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6LetsReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int test = 0; test < n; test++)
+            {
+                string s = Console.ReadLine();
+
+                //string test = "Hacker";
+
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(s[i]);
+                    }
+                }
+
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (j % 2 == 1)
+                    {
+                        Console.Write(s[j]);
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
