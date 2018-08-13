427b559f514c1e9893c9c888348bd9da8092beab
Maxyee <eyaminkhan00@gmail.com>
2018-08-14 02:14:25 +0600

    Learned code how to find 2nd large number form an Array

---
+++ CSharpChallenges/SecoundLargeValue/SecoundLarge.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecoundLargeValue
+{
+    class SecoundLarge
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine("Enter the size of the array");
+            size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+
+            for (int i = 0; i<size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int length = arr.Length;
+
+            SecoundLarge secound = new SecoundLarge();
+            secound.SecoundlargeValue(arr, length);
+
+            Console.ReadKey();
+
+        }
+
+        private void SecoundlargeValue(int[] arr, int length)
+        {
+            int maxValue = 0;
+            int secondMaxValue = 0;
+
+            for(int i = 0; i < length; i++)
+            {
+                if(arr[i] > maxValue)
+                {
+                    secondMaxValue = maxValue;
+                    maxValue = arr[i];
+                }
+                else if(arr[i] > secondMaxValue)
+                {
+                    secondMaxValue = arr[i];
+                }
+            }
+            Console.WriteLine("First Largest number :" + maxValue);
+            Console.WriteLine("Second Largest number :" + secondMaxValue);
+            Console.ReadLine();
+        }
+    }
+}
